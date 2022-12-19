fdca3f3aef290f5b3f9be889e99c1a0fa867711a
Sonudyade <112577115+Sonudyade@users.noreply.github.com>
2022-12-19 20:14:51 +0530

    Update Test.java

--- spring/util/Test.java
+++ spring/util/Test.java
@@ -9,8 +9,8 @@ public class Test {
 		Resource r=new ClassPathResource("applicationContext.xml");  
 		BeanFactory b=new XmlBeanFactory(r);  
 		QAMailsender m=(QAMailsender)b.getBean("qAMailsender");  
-		String sender="98sonykumari@gmail.com";//write here sender gmail id  
-		String receiver="anitashankar86@gmail.com";//write here receiver id  
+		String sender="************";//write here sender gmail id  
+		String receiver="*************";//write here receiver id  
 		m.sendMail(sender,receiver,"hi","welcome");  
 		      
 		System.out.println("success");  
